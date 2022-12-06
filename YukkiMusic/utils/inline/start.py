3d68660e22f34cb51707db3e9322410bce03fca0
Kassim darlzzz <117517237+Kassim-the-devil@users.noreply.github.com>
2022-12-06 21:26:55 +0530

    Update start.py

--- YukkiMusic/utils/inline/start.py
+++ YukkiMusic/utils/inline/start.py
@@ -97,7 +97,7 @@ def private_panel(_, BOT_USERNAME, OWNER: Union[bool, int] = None):
     buttons.append(
         [
             InlineKeyboardButton(
-                text=_["𝐀ᴅᴅ 𝐌ᴇ 𝐓ᴏ 𝐘ᴏᴜʀ 𝐆ʀᴏᴜᴘ"],
+                text=_["S_B_6"],
                 url=f"https://t.me/{BOT_USERNAME}?startgroup=true",
             )
         ]
@@ -124,7 +124,7 @@ def private_panel(_, BOT_USERNAME, OWNER: Union[bool, int] = None):
             buttons.append(
                 [
                     InlineKeyboardButton(
-                        text=_["𝐎ᴡɴᴇʀ"], user_id=OWNER
+                        text=_["S_B_6"], user_id=OWNER
                     ),
                 ]
             )
